9c594eb520dcf9783b62d9719c921a8d75e1c2f6
masa-fullstack <ishida.masahidee@gmail.com>
2022-04-04 20:03:06 +0900

    add linkdin

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
           {isProduction && (
             <>
               <script
